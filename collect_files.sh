3b835b50f6f13ffd0ed282f87c333134d0d080d3
root <root@yjeuigcrer.local>
2025-04-27 11:45:48 +0000

    result

--- collect_files.sh
+++ collect_files.sh
@@ -1,6 +1,16 @@
 #!/bin/bash
-input_dir=$1
-output_dir=$2
-max_depth=$3
+if [[ $3 == "--max_depth" ]]; then
+    input_dir=$1
+    output_dir=$2
+    max_depth=$4
+elif [[ $1 == "--max_depth" ]]; then
+    max_depth=$2
+    input_dir=$3
+    output_dir=$4
+else
+    input_dir=$1
+    output_dir=$2
+    max_depth=$3
+fi
 export input_dir output_dir max_depth
 python3 collect_files.py
